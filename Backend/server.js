0e37b5247e00b673025b9d28f62b169658f38654
Partha Maity <parthamaity2004@gmail.com>
2025-09-19 00:09:26 +0530

    Process env port

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ import cookieParser from "cookie-parser";
 
 
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
